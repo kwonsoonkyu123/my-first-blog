a9cbefa433ef485f22e1601f5ad5680b745c287c
nero <nerochaos2@korea.ac.kr>
2017-09-01 11:45:12 +0900

    make template

--- why/views.py
+++ why/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'why/post_list.html', {})
+    posts = Post.objects.order_by('-title')
+    return render(request, 'why/post_list.html', {'posts' : posts})
